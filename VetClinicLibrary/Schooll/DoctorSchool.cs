ac4b1616cf9ee90808ce687656cfeb87276832b8
Ertan Taner <ertantaner2000@gmail.com>
2022-12-20 17:49:59 +0300

    fix: doctor class added

---
+++ VetClinicLibrary/Schooll/DoctorSchool.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VetClinicLibrary.Schooll;
+
+public class DoctorSchool
+{
+    // composite key
+    public int DoctorId { get; set; }
+    public int SchoolId { get; set; }
+}
