c9b9a04b25ec6b8aa2e8be259c704ff95939d5bd
Ertan Taner <ertantaner2000@gmail.com>
2022-12-26 14:09:31 +0300

    refactor: remove animalclass and add navigation to interface

--- VetClinicLibrary/Animall/IAnimal.cs
+++ VetClinicLibrary/Animall/IAnimal.cs
@@ -4,9 +4,12 @@ using VetClinicLibrary.Pett.Speciee;
 namespace VetClinicLibrary.Animall;
 
 public interface IAnimal
-{  
+{
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public int SpecieId { get; set; }
     public int BreedId { get; set; }
+    public Specie Specie { get; set; }
+    public Breed Breed { get; set; }
+
 }
