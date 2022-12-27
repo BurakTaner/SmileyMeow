c462d242a8eae54959a55461491eec81c02721a8
Ertan Taner <ertantaner2000@gmail.com>
2022-12-27 18:00:48 +0300

    feat: add doctorpreference and navigations

--- VetClinicLibrary/Appointmentt/IAppointment.cs
+++ VetClinicLibrary/Appointmentt/IAppointment.cs
@@ -1,15 +1,20 @@
 using System;
 using VetClinicLibrary.Appointmentt.AppointmentStatuss;
+using VetClinicLibrary.Person;
+using VetClinicLibrary.PetnPersonn;
 
 namespace VetClinicLibrary.Appointmentt.StatusLevell;
 
 public interface IAppointment
 {
     int PetnPersonId { get; set; }
+    PetnPerson PetnPerson { get; set; }
     int DoctorId { get; set; }
+    Doctor Doctor { get; set; }
     DateTime TimeCreated { get; set; }
     DateTime AppointmentDate { get; set; }
     int AppointmentStatussId { get; set;  }
     AppointmentStatus AppointmentStatus { get; set;}
-    
+    int DoctorPreferenceId { get; set; }
+    Doctor DoctorPreference { get; set; }
 }
