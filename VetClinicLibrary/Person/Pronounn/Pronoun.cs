5c72b2b262a6e1b64281c92f7fb26ad2e7f72d25
Ertan Taner <ertantaner2000@gmail.com>
2023-01-06 23:58:02 +0300

    feat: add crud pronoun

--- VetClinicLibrary/Person/Pronounn/Pronoun.cs
+++ VetClinicLibrary/Person/Pronounn/Pronoun.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace VetClinicLibrary.Person.Prounounn;
 public class Pronoun : IPronoun
 {
     public int ProunounId { get; set; }
+    [Required(ErrorMessage = "{0} can't be empty"),Display(Name = "Pronoun"), StringLength(20 ,MinimumLength = 7 , ErrorMessage = "{0} must be between {2} - {1}")]
     public string PName { get; set; }
     public List<Doctor> Doctors { get; set; }
     public List<PetParent> PetParents { get; set; }
