b6b487fa8799af3bcbe12d2e77ac2112865407ff
Ertan Taner <ertantaner2000@gmail.com>
2023-01-09 11:36:32 +0300

    chore: change form name

--- SmileyMeow/wwwroot/js/adminpages/forms/petadoptionform.js
+++ SmileyMeow/wwwroot/js/adminpages/forms/petadoptionform.js
@@ -3,7 +3,7 @@ import { templateForm } from "../../modules/formgeneral.js";
 $(function () {
   const idElem = $('#animalid');
   const id = idElem.val();
-  const entityName = "petadopt";
+  const entityName = "petadoption";
 
   templateForm(id,entityName,idElem);  
 });
