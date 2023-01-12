55d475b293cf5cb2044f8a1f53d4bed6801ca595
Ertan Taner <ertantaner2000@gmail.com>
2023-01-12 20:52:56 +0300

    fix: dto location and length

--- SmileyMeow/DTOs/AdoptionTextDTO.cs
+++ SmileyMeow/DTOs/AdoptionTextDTO.cs
@@ -8,7 +8,7 @@ namespace SmileyMeow.DTOs
 {
     public class AdoptionTextDTO
     {
-        [Required(ErrorMessage = "{0} can't be empty"),Display(Name = "AdoptionText"), StringLength(150 ,MinimumLength = 30 , ErrorMessage = "{0} must be between {2} - {1}")]
+        [Required(ErrorMessage = "{0} can't be empty"),Display(Name = "AdoptionText"), StringLength(350 ,MinimumLength = 30 , ErrorMessage = "{0} must be between {2} - {1}")]
         public string AdoptionText { get; set; }
     }
 }
